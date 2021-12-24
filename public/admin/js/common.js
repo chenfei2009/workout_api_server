cb287117fbac8706d2588a9dd27584c21c8897fb
chenfei2009 <chenfei_la@163.com>
2021-12-24 14:23:46 +0800

    确认修改

--- public/admin/js/common.js
+++
@@ -1,9 +0,0 @@
-// 表单数据格式化
-function serializeToJson(form) {
-  let result = {};
-  let array = form.serializeArray();
-  array.forEach(v => {
-    result[v.name] = v.value;
-  })
-  return result;
-}
